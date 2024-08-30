3fe38b622ef7b001baaf573059112192a5ef11f5
Pushan <mukhopadhyaypushan42@gmail.com>
2024-08-31 02:05:11 +0530

    add phone call function

--- lib/widgets/home_widgets/emergencies/ambulence_emergency.dart
+++ lib/widgets/home_widgets/emergencies/ambulence_emergency.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 class AmbulenceEmergency extends StatelessWidget {
   const AmbulenceEmergency({super.key});
+
+  _callNumber(String number) async {
+    await FlutterPhoneDirectCaller.callNumber(number);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,71 +17,78 @@ class AmbulenceEmergency extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Container(
-          height: 200,
-          width: MediaQuery.of(context).size.width * 0.7,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFFFD8080),
-                Color(0xFFFB8580),
-                Color(0xFFFBD079),
-              ],
+        child: InkWell(
+          onTap: () {
+            _callNumber("108");
+          },
+          child: Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFFFD8080),
+                  Color(0xFFFB8580),
+                  Color(0xFFFBD079),
+                ],
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.white.withOpacity(0.5),
-                  child: Image.asset('assets/ambulance.png'),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Ambulence",
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width * 0.06,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )),
-                      Text("Call 1-0-8 for medical emergency",
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width * 0.045,
-                            overflow: TextOverflow.ellipsis,
-                            color: Colors.white,
-                          )),
-                      Container(
-                        height: 30,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "1-0-8",
-                            style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.045,
-                                overflow: TextOverflow.ellipsis,
-                                color: Colors.red[300],
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      )
-                    ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white.withOpacity(0.5),
+                    child: Image.asset('assets/ambulance.png'),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Ambulence",
+                            style: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.06,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            )),
+                        Text("Call 1-0-8 for medical emergency",
+                            style: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.045,
+                              overflow: TextOverflow.ellipsis,
+                              color: Colors.white,
+                            )),
+                        Container(
+                          height: 30,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "1-0-8",
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.045,
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Colors.red[300],
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/home_widgets/emergencies/fire_brigade.dart
+++ lib/widgets/home_widgets/emergencies/fire_brigade.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 class FirebrigadeEmergency extends StatelessWidget {
   const FirebrigadeEmergency({super.key});
+
+  callNumber(String number) async {
+    await FlutterPhoneDirectCaller.callNumber(number);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,71 +17,78 @@ class FirebrigadeEmergency extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Container(
-          height: 200,
-          width: MediaQuery.of(context).size.width * 0.7,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFFFD8080),
-                Color(0xFFFB8580),
-                Color(0xFFFBD079),
-              ],
+        child: InkWell(
+          onTap: () {
+            callNumber("101");
+          },
+          child: Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFFFD8080),
+                  Color(0xFFFB8580),
+                  Color(0xFFFBD079),
+                ],
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.white.withOpacity(0.5),
-                  child: Image.asset('assets/flame.png'),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Fire Brigade",
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width * 0.06,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )),
-                      Text("For fire emergency call 1-0-1",
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width * 0.045,
-                            overflow: TextOverflow.ellipsis,
-                            color: Colors.white,
-                          )),
-                      Container(
-                        height: 30,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "1-0-1",
-                            style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.045,
-                                overflow: TextOverflow.ellipsis,
-                                color: Colors.red[300],
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      )
-                    ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white.withOpacity(0.5),
+                    child: Image.asset('assets/flame.png'),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Fire Brigade",
+                            style: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.06,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            )),
+                        Text("For fire emergency call 1-0-1",
+                            style: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.045,
+                              overflow: TextOverflow.ellipsis,
+                              color: Colors.white,
+                            )),
+                        Container(
+                          height: 30,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "1-0-1",
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.045,
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Colors.red[300],
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
