7a55ee525f8f73a9f0baa25e9c9c44ef4df7d615
Pushan <mukhopadhyaypushan42@gmail.com>
2024-10-14 01:24:42 +0530

    Contact read access permission done

--- lib/child/bottom_screens/contact_page.dart
+++ lib/child/bottom_screens/contact_page.dart
@@ -1,12 +1,79 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:woman_safety/constant.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
 
   @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  List<Contact> contacts = [];
+
+  void initState() {
+    super.initState();
+    askpermission();
+  }
+
+  Future<void> askpermission() async {
+    PermissionStatus status = await getContactsPermission();
+    if (status == PermissionStatus.granted) {
+      getAllContacts();
+    } else {
+      handleInvalidPermissions(status);
+    }
+  }
+
+  getAllContacts() async {
+    List<Contact> _contacts = (await ContactsService.getContacts()).toList();
+    setState(() {
+      contacts = _contacts;
+    });
+  }
+
+  void handleInvalidPermissions(PermissionStatus status) {
+    if (status.isPermanentlyDenied) {
+      openAppSettings();
+    }
+  }
+
+  Future<PermissionStatus> getContactsPermission() async {
+    PermissionStatus status = await Permission.contacts.status;
+    if (!status.isGranted || status.isPermanentlyDenied) {
+      status = await Permission.contacts.request();
+
+      if (status.isPermanentlyDenied) {
+        openAppSettings();
+      }
+    }
+    return status;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Contact Page")),
+    return SafeArea(
+      child: Scaffold(
+        body: contacts.length == 0
+            ? Center(
+                child: Progress(),
+              )
+            : ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(contacts[index].displayName ?? ""),
+                    subtitle:
+                        Text(contacts[index].phones?.elementAt(0).value ?? ""),
+                    leading: CircleAvatar(
+                      child: Text(contacts[index].initials()),
+                    ),
+                  );
+                },
+                itemCount: contacts.length,
+              ),
+      ),
     );
   }
 }
